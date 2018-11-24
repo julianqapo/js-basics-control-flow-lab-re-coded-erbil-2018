0b770f14728dc4f2ef6f21aeafb08896dcffa4d5
Julian M. Qapo <julian.qapo@gmail.com>
2018-11-24 13:48:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+function scuberGreetingForFeet(){
+  
+}
+
+
+
+function ternaryCheckCity()(city) {
+  return (city ? "Ok, sounds good." : "No go.");
+console.log(ternaryCheckCity(city));
+}
